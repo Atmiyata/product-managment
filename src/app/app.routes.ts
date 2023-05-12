d3eed315dafff0f92d500cafec498eef129c964c
Atmiyata <jayrajsolanki50@gmail.com>
2023-05-11 22:10:29 -0400

    router link with layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: '',
+    path: 'products',
     component: ProductsComponent,
   },
   {
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
