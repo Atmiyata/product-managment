85b8ff81ebaa8be2528fab98f1228721ffba252e
Atmiyata <jayrajsolanki50@gmail.com>
2023-05-13 11:30:13 -0400

    confirm dialog for soft delete

--- src/app/components/products/product-card/product-card.component.ts
+++ src/app/components/products/product-card/product-card.component.ts
@@ -11,9 +11,11 @@ import {
 
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 import { Product } from 'src/app/shared/models/product';
 import { ProductService } from 'src/app/shared/services/product.service';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-product-card',
@@ -24,19 +26,33 @@ import { ProductService } from 'src/app/shared/services/product.service';
     RatingModule,
     ButtonModule,
     NgOptimizedImage,
+    ConfirmDialogModule,
   ],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
+  providers: [ConfirmationService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCardComponent {
   @Input({ required: true }) product!: Product;
   @Output() onSoftDelete: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private confirmationService: ConfirmationService
+  ) {}
 
   softDelete(id: string) {
+    console.log('called');
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.onSoftDelete.emit(id);
+      },
+      reject: () => {},
+    });
     //Todo - add confirm modal
-    this.onSoftDelete.emit(id);
   }
 }
